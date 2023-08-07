31382637bd37110688425998152ad6423295c701
Azhar564 <azharramadhan7777@gmail.com>
2023-08-07 23:16:51 +0700

    add Scene Position ID

--- Assets/Scripts/LoadCharacter.cs
+++ Assets/Scripts/LoadCharacter.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[System.Serializable]
+public struct CharacterSpawnData
+{
+    public int id;
+    public Transform targetPos;
+}
 public class LoadCharacter : MonoBehaviour
 {
     public GameObject[] characterPrefabs;
-    public Transform spawnPoint;
+    public List<CharacterSpawnData> spawnDataPoints = new List<CharacterSpawnData>();
     public TMP_Text label;
 
     void Start()
     {
         int selectedCharacter = PlayerPrefs.GetInt("selectedCharacter");
         GameObject prefab = characterPrefabs[selectedCharacter];
-        GameObject clone = Instantiate(prefab, spawnPoint.position, Quaternion.identity);
-        label.text = prefab.name;
 
+        Vector3 targetPos = Vector3.zero;
+        foreach (CharacterSpawnData spawnData in spawnDataPoints)
+        {
+            if (spawnData.id == LoadSceneBehaviour.targetID)
+            {
+                targetPos = spawnData.targetPos.position;
+            }
+        }
+        GameObject clone = Instantiate(prefab, targetPos, Quaternion.identity);
+
+        label.text = prefab.name;
         StartCoroutine(HideTextAfterDelay(10f)); // Memulai coroutine untuk menyembunyikan teks setelah 10 detik
+
     }
 
     IEnumerator HideTextAfterDelay(float delay)
--- Assets/Scripts/LoadSceneBehaviour.cs
+++ Assets/Scripts/LoadSceneBehaviour.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoadScene : MonoBehaviour
+public class LoadSceneBehaviour : MonoBehaviour
 {
+    public static int targetID;
     public string sceneName;
     public void LoadSceneWithName()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void SetID(int id)
+    {
+        targetID = id;
     }
 }
